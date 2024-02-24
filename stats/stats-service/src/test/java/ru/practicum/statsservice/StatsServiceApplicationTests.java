e25dc93341af657736711ae218ab83f7f4059ec8
evgenii <gulllak@yandex.ru>
2024-02-24 23:32:16 +0300

    fix: test

--- stats/stats-service/src/test/java/ru/practicum/statsservice/StatsServiceApplicationTests.java
+++ stats/stats-service/src/test/java/ru/practicum/statsservice/StatsServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.practicum.statsservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StatsServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
